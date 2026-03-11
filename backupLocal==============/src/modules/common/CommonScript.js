393a25896eab41e6231cd99ecfa85b7a88ab237f
Nomaan-i2a <noman.na@akounto.com>
2026-03-11 19:27:26 +0530

    new

--- backupLocal==============/src/modules/common/CommonScript.js
+++ backupLocal==============/src/modules/common/CommonScript.js
@@ -31,7 +31,7 @@ function CommonScript() {
               observer.unobserve(img);
             }
           });
-        }, { rootMargin: "0px 0px 0px 0px" });
+        }, { rootMargin: "0px 0px -100px 0px" });
         document.querySelectorAll('img[data-src]').forEach(img => { 
           observer.observe(img);
         });
@@ -55,7 +55,7 @@ function CommonScript() {
       document.querySelectorAll(".partition img").forEach(img => img.addEventListener("load", resizeImage));
     }
 
-    
+
     // {}
     // {}
     // {}
