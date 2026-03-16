75f4ab0a40ec51928841c5911180f56697f2bff1
Nomaan-i2a <noman.na@akounto.com>
2026-03-16 18:04:35 +0530

    new

--- backupLocal==============/src/App.js
+++ backupLocal==============/src/App.js
@@ -16,7 +16,7 @@ import ThankYou from "./modules/ThankYou";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <RenderTop />
       <CommonScript />
       <Header />
@@ -32,7 +32,7 @@ function App() {
       <Footer />
       <ScrollTop />
 
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
