28426b147f1e18c0ea4b072c8ca0b3b7f5fdc2d9
Nomaan-i2a <noman.na@akounto.com>
2026-03-17 17:13:37 +0530

    new

--- backupLocal==============/src/modules/AboutUs/CounterNum.js
+++ backupLocal==============/src/modules/AboutUs/CounterNum.js
@@ -1,36 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 
 const CounterNum = ({ end, prefix = "", suffix = "", className = "" }) => {
-  const [startCount, setStartCount] = useState(false);
-
   const { ref, inView } = useInView({
-    threshold: 0.5,
     triggerOnce: true,
+    threshold: 0.2,
   });
-
-  useEffect(() => {
-    if (inView) {
-      setStartCount(true);
-    }
-  }, [inView]);
 
   return (
     <div ref={ref} className={className}>
-      {startCount && (
-        <CountUp
-          start={0}
-          end={end}
-          duration={5}
-          prefix={prefix}
-          suffix={suffix}
-        >
-          {({ countUpRef }) => (
-            <span ref={countUpRef} />
-          )}
-        </CountUp>
-      )}
+      <CountUp
+        start={inView ? 0 : null}
+        end={end}
+        duration={3}
+        prefix={prefix}
+        suffix={suffix}
+      />
     </div>
   );
 };
