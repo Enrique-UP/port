8f0546654e81bac2fad4afce20505f64fce240fe
Nomaan-i2a <noman.na@akounto.com>
2026-04-06 20:42:53 +0530

    new

--- backup================/src/modules/about/AboutHome.jsx
+++ backup================/src/modules/about/AboutHome.jsx
@@ -60,48 +60,37 @@ export default function AboutHome() {
     <>
       <section className="section about">
         <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <hgroup>
-                <h2>About Us</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit minus unde</p>
-              </hgroup>
-            </div>
-            <div className="col-12 space">
-              <div className="fw">
-                <ul className="images">
-                  <li><img className="img1" src={img1} alt="" /></li>
-                  <li><img className="img1" src={img2} alt="" /></li>
-                  <li><img className="img2" src={img3} alt="" /></li>
+          <hgroup>
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit minus unde</p>
+          </hgroup>
+          <div className="fw">
+            <div className="cols1">
+              <ul className="images">
+                <li><img className="img1" src={img1} alt="" /></li>
+                <li><img className="img1" src={img2} alt="" /></li>
+                <li><img className="img2" src={img3} alt="" /></li>
+              </ul>
+            </div>{/* cols1 */}
+            <div className="cols2">
+              <div className="cnt">
+                <p className="int">Hello!</p>
+                <p className="int">P Lm Loremm Ipum,</p>
+                <p className="post"><TypeWriter words={words1} /></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia necessitatibus esse! Asperiores dolorum veritatis quam, praesentium incidunt excepturi neque perferendis totam, natus libero corporis sed aut temporibus, harum explicabo unde minus doloribus. Maxime totam distinctio aspernatur recusandae dolor fugit excepturi at, esse tenetur magni eos consequuntur ad iure optio.</p>
+              </div>{/* cnt */}
+              <div className="cnt">
+                <p className="hd">Heading</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+                <ul className="lists one">
+                  <ListContent />
                 </ul>
-              </div>{/* fw */}
-              <div className="fw">
-                <div className="cnt">
-                  <p className="int">Hello!</p>
-                  <p className="int">P Lm Loremm Ipum,</p>
-                  <p className="post"><TypeWriter words={words1} /></p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia necessitatibus esse! Asperiores dolorum veritatis quam, praesentium incidunt excepturi neque perferendis totam, natus libero corporis sed aut temporibus, harum explicabo unde minus doloribus. Maxime totam distinctio aspernatur recusandae dolor fugit excepturi at, esse tenetur magni eos consequuntur ad iure optio.</p>
-                </div>{/* cnt */}
-              
-                <div className="cnt">
-                  <p className="hd">Heading</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-                  <ul className="lists one">
-                    <ListContent />
-                  </ul>
-                  <ul className="lists two">
-                    <ListContent />
-                  </ul>
-                </div>{/* cnt */}
-              </div>{/* fw */}
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="section about">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
+              </div>{/* cnt */}
+            </div>{/* cols2 */}
+            <div className="cols3">
+              <ul className="lists two">
+                  <ListContent />
+                </ul>
               <div className="cnt">
                 <p className="hd">Heading</p>
                 <div className="skillBar colors one">
@@ -147,10 +136,8 @@ export default function AboutHome() {
                   </article>
                 </div>
               </div>{/* cnt */}
-            </div>{/* cols */}
-            <div className="col-12">
               <div className="cnt">
-                <p className="hd">Headingsss</p>
+                <p className="hd">Heading</p>
                 <div className="skillBar colors two">
                   <article>
                     <b>Lorem</b>
@@ -168,22 +155,17 @@ export default function AboutHome() {
                     <i></i>
                   </article>
                 </div>
-              </div>
-            </div>{/* cols */}
-            <div className="col-12">
+              </div>{/* cnt */}
               <div className="cnt">
                 <p className="hd">Heading</p>
                 <p>Lorem ipsum dolor sit <b>amet consectetur adipisicing</b> elit. Recusandae non quibusdam aliquid! Officia dolorem dolorum nesciunt illo voluptates itaque, iure, ipsam explicabo blanditiis eum maiores eos aut. Deserunt fuga, minima fugiat voluptatum ullam esse repellendus architecto repudiandae. Ratione neque impedit eaque ipsum consequatur, enim in corporis dolore veritatis atque, voluptatem recusandae et? Id illo doloribus, ullam aliquid quis placeat rerum hic voluptates et assumenda explicabo recusandae cupiditate repellendus numquam sed enim. Amet delectus cum nostrum sunt dolores distinctio similique commodi sequi suscipit ut. Exercitationem similique maxime, dicta obcaecati impedit eaque sed dignissimos rerum maiores numquam beatae quam libero tempore ipsam?</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae non quibusdam aliquid! Officia dolorem dolorum nesciunt illo voluptates itaque, iure, ipsam explicabo blanditiis eum maiores eos aut. Deserunt fuga, minima fugiat voluptatum ullam esse repellendus architecto repudiandae. Ratione neque impedit eaque ipsum consequatur, enim in corporis dolore veritatis atque, voluptatem recusandae et? Id illo doloribus, ullam aliquid quis placeat rerum hic voluptates et assumenda explicabo recusandae cupiditate repellendus numquam sed enim. Amet delectus cum nostrum sunt dolores distinctio similique commodi sequi suscipit ut. Exercitationem similique maxime, dicta obcaecati impedit eaque sed dignissimos rerum maiores numquam beatae quam libero tempore ipsam?</p>
-              </div>
-            </div>{/* cols */}
-            <div className="col-12">
+              </div>{/* cnt */}
               <div className="cnt">
                 <p className="hd">Heading</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae non quibusdam aliquid! Officia dolorem dolorum nesciunt illo voluptates itaque, iure, ipsam explicabo blanditiis eum maiores eos aut. Deserunt fuga, minima fugiat voluptatum ullam esse repellendus architecto repudiandae. Ratione neque impedit eaque ipsum consequatur, enim in corporis dolore veritatis atque, voluptatem recusandae et? Id illo doloribus, ullam aliquid quis placeat rerum hic voluptates et assumenda explicabo recusandae cupiditate repellendus numquam sed enim. Amet delectus cum nostrum sunt dolores distinctio similique commodi sequi suscipit ut. Exercitationem similique maxime, dicta obcaecati impedit eaque sed dignissimos rerum maiores numquam beatae quam libero tempore ipsam?</p>
-              </div>
-            </div>{/* cols */}
-            <div className="col-12">
+              </div>{/* cnt */}
+
               <ul className="counterBox">
                 <li>
                   <i className="icon">&#xa001;</i>
@@ -206,21 +188,19 @@ export default function AboutHome() {
                   <CounterNum target={20} suffix="+" />
                 </li>
               </ul>
-            </div>{/* cols */}
-            <div className="col-12">
+
               <div className="outlineBox">
                 <b>Lorem</b>
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero neque quasi et quos reiciendis earum ratione excepturi. Amet nam temporibus deleniti adipisci, fuga fugit eos placeat reprehenderit a corporis vel pariatur unde hic eveniet minus, necessitatibus, qui dolore sapiente in consequuntur sunt magnam ipsum? Eaque aut animi suscipit a, ducimus ab perferendis harum error sit illo iusto tempora perspiciatis quos at quas quis consequuntur dolorum impedit quibusdam corporis totam. Ducimus quam officiis commodi placeat voluptate ab cumque maxime tempore, adipisci sint quia, omnis animi non facilis enim ipsa deleniti ea doloremque nisi vero ullam repellat alias odit iste? Eveniet, totam.</span>
-              </div>
-            </div>{/* cols */}
-            <div className="col-12">
+              </div>{/* outlineBox */}
+
               <div className="btns center">
                 <a href={Global.numTel} target="_blank">Hire Us</a>
                 <Link href="/contactus">Contact Us</Link>
-              </div>
-            </div>{/* cols */}
-          </div>
-        </div>{/* container */}
+              </div>{/* btns */}
+            </div>{/* cols3 */}
+          </div>{/* fw */}
+        </div>
       </section>
     </>
   );
