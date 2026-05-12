2b71640ae0c46cf291433ec472ad47a558686615
nomaanarif786 <noman.na@akounto.com>
2026-05-12 16:25:11 +0530

    new

--- backup===================/src/modules/lifeStyle/LifeStyleTextDate.jsx
+++ backup===================/src/modules/lifeStyle/LifeStyleTextDate.jsx
@@ -1,27 +1,12 @@
-// const LifeStyleTextDate = [
-//   { hd: "Celebrate Diwali", shd: "Festive Mood" },
-//   { hd: "Heritage Walks", shd: "History Lover" },
-//   { hd: "Christmas Vibes", shd: "Holiday Mood" },
-//   { hd: "Mountain View", shd: "Manali Stay" },
-//   { hd: "Himalayan Peace", shd: "Spiritual Vibes" },
-//   { hd: "Peaceful Escape", shd: "Into Nature" },
-//   { hd: "Nature Walks", shd: "River Views" },
-//   { hd: "Beach Life", shd: "Ocean Mood" },
-//   { hd: "Wedding Look", shd: "Wedding Vibes" },
-// ];
-// export default LifeStyleTextDate;
-
-
 const LifeStyleTextDate = [
-  { hd: "Diwali Celebration", shd: "Festive Glow" },
+  { hd: "Diwali Celebration", shd: "Festive Mood" },
   { hd: "Heritage Walk", shd: "Historic Vibes" },
-  { hd: "Christmas Moments", shd: "Holiday Cheer" },
-  { hd: "Mountain Escape", shd: "Manali Views" },
-  { hd: "Himalayan Calm", shd: "Spiritual Journey" },
-  { hd: "Nature Escape", shd: "Peaceful Mind" },
-  { hd: "Nature Trails", shd: "River Flow Views" },
-  { hd: "Beach Vibes", shd: "Ocean Breeze" },
-  { hd: "Wedding Attire", shd: "Elegant Look" },
+  { hd: "Christmas Vibes", shd: "Holiday Mood" },
+  { hd: "Mountain Views", shd: "Manali Stay" },
+  { hd: "Himalayan Peace", shd: "Spiritual Vibe" },
+  { hd: "Peaceful Escape", shd: "Into Nature" },
+  { hd: "Nature Walk", shd: "River Views" },
+  { hd: "Beach Life", shd: "Ocean Mood" },
+  { hd: "Wedding Look", shd: "Wedding Vibes" },
 ];
-
 export default LifeStyleTextDate;
